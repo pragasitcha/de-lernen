e4f7b95d5943b1a9a6579f8cf99f944884a2dcad
pragasitcha <rasetsacrifa@gmail.com>
2021-10-15 11:48:50 +0700

    Update verben.py

--- verben.py
+++ verben.py
@@ -10,10 +10,27 @@ wb_obj = openpyxl.load_workbook(xlsx_file)
 vbn_sheet = wb_obj[verven_tab]
 print("========= start =========")
 row_count = 1
+prepend_first = 1
+default_first = prepend_first+1
+default_last = 30
+
+input_first_row = input("select first row number:")
+input_last_row = input("select last row number:")
+
+if input_first_row != "" and input_first_row != 0 :
+	input_first_row = int(input_first_row)
+else:
+	input_first_row = default_first
+	
+if input_last_row != "" and input_last_row != 0 :
+	input_last_row = int(input_last_row) - 1
+else:
+	input_last_row = default_last
+
 
 for row in vbn_sheet.iter_rows(values_only=True,
-								min_row=2,
-								max_row=3,
+								min_row= int(input_first_row) ,
+								max_row= int(input_last_row),
 								max_col=3):
 	print("##### row:"+str(row_count))
 	while True:	
@@ -43,6 +60,8 @@ for row in vbn_sheet.iter_rows(values_only=True,
 		
 		if next != ("n") or next != ("N"):
 			break
+		else:
+			continue
 			
 	row_count=row_count+1
 		
